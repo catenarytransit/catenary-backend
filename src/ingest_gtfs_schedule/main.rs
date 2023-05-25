426d8a3f12f3d49e7bb47423cd892fbc4d42446f
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-05-24 21:11:55 -0700

    broken downloads

--- src/ingest_gtfs_schedule/main.rs
+++ src/ingest_gtfs_schedule/main.rs
@@ -1,8 +1,12 @@
 use std::error::Error;
-use std::fs::File;
 use std::io::prelude::*;
 use csv::{ReaderBuilder, ErrorKind};
 use serde::Deserialize;
+
+use std::fs::File;
+use std::io::copy;
+use reqwest;
+use std::path::Path;
 
 #[derive(Debug, Deserialize)]
 struct Agency {
@@ -11,7 +15,7 @@ struct Agency {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut file = File::open("./../../gtfs_schedules.csv")?;
+    let mut file = File::open("./gtfs_schedules.csv")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
@@ -24,17 +28,38 @@ fn main() -> Result<(), Box<dyn Error>> {
         let record: Agency = match result {
             Ok(record) => record,
             Err(err) => {
-                if err.kind() == ErrorKind::Deserialize {
-                    continue;
-                } else {
                     return Err(Box::new(err));
-                }
             }
         };
         agencies.push(record);
     }
 
-    println!("{:#?}", agencies);
+    //sprintln!("{:#?}", agencies);
+
+    for agency in agencies {
+        println!("{}: {}", agency.agency, agency.url);
+
+        //make dir called temp
+        //download gtfs into temp directory
+
+        let file_path = Path::new("temp").join(agency.agency + "-gtfs.zip");
+        let mut file = File::create(file_path)?;
+
+        let mut response = reqwest::get(agency.url);
+        match response {
+            Ok(mut response) => {
+                match copy(&mut response, &mut file) {
+                    Ok(_) => {
+                        println!("Downloaded {} gtfs", agency.agency);
+                        copy(&mut response, &mut file)?;
+                    },
+                    Err(err) => println!("Error downloading {}: {}", agency.agency, err),
+                }
+            },
+            Err(err) => println!("Error downloading {}: {}", agency.agency, err),
+        };
+        
+    }
 
     Ok(())
 }
