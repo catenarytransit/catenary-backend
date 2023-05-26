d43fd52ff37c7d1963198c8200e979df9bf8c98d
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-05-25 23:14:29 -0700

    actual download!

--- src/ingest_gtfs_schedule/main.rs
+++ src/ingest_gtfs_schedule/main.rs
@@ -1,12 +1,19 @@
 use std::error::Error;
 use std::io::prelude::*;
-use csv::{ReaderBuilder, ErrorKind};
+use csv::{ReaderBuilder};
+use serde::ser::StdError;
 use serde::Deserialize;
 
 use std::fs::File;
-use std::io::copy;
-use reqwest;
 use std::path::Path;
+use reqwest;
+use tokio::task::JoinHandle;
+
+use std::io::{Read, Write};
+use std::net::TcpStream;
+
+#[feature(async_await)]
+use futures::future::{join_all};
 
 #[derive(Debug, Deserialize)]
 struct Agency {
@@ -14,10 +21,11 @@ struct Agency {
     url: String,
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let mut file = File::open("./gtfs_schedules.csv")?;
+#[tokio::main]
+async fn main() -> Result<(), Box<csv::Error>> {
+    let mut file = File::open("./gtfs_schedules.csv");
     let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
+    file.expect("read zip file failed!").read_to_string(&mut contents);
 
     let mut reader = ReaderBuilder::new()
         .has_headers(true)
@@ -34,32 +42,37 @@ fn main() -> Result<(), Box<dyn Error>> {
         agencies.push(record);
     }
 
-    //sprintln!("{:#?}", agencies);
+    // Iterate over the paths.
+    let mut tasks: Vec<JoinHandle<Result<(), ()>>>= vec![];
 
     for agency in agencies {
-        println!("{}: {}", agency.agency, agency.url);
+        // Copy each path into a new string
+    // that can be consumed/captured by the task closure
+    let path = agency.url.clone();
 
-        //make dir called temp
-        //download gtfs into temp directory
+    // Create a Tokio task for each path
+    tasks.push(tokio::spawn(async move {
+        match reqwest::get(&path).await {
+            Ok(resp) => {
+                match resp.text().await {
+                    Ok(text) => {
+                        println!("RESPONSE: {} KB from {}", text.len()/1000, path);
 
-        let file_path = Path::new("temp").join(agency.agency + "-gtfs.zip");
-        let mut file = File::create(file_path)?;
-
-        let mut response = reqwest::get(agency.url);
-        match response {
-            Ok(mut response) => {
-                match copy(&mut response, &mut file) {
-                    Ok(_) => {
-                        println!("Downloaded {} gtfs", agency.agency);
-                        copy(&mut response, &mut file)?;
-                    },
-                    Err(err) => println!("Error downloading {}: {}", agency.agency, err),
+                        let gtfs = gtfs_structures::Gtfs::new(&path)?;
+                        println!("there are {} stops in the gtfs", gtfs.stops.len());
+                    }
+                    Err(_) => println!("ERROR reading {}", path),
                 }
-            },
-            Err(err) => println!("Error downloading {}: {}", agency.agency, err),
-        };
-        
+            }
+            Err(_) => println!("ERROR downloading {}", path),
+        }
+        Ok(())
+    }));
     }
+
+    // Wait for them all to finish
+    println!("Started {} tasks. Waiting...", tasks.len());
+    join_all(tasks).await;
 
     Ok(())
 }
