020f08621f37e85d751db19e3ce7d27b6cc9c0e3
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-07-05 20:37:39 -0700

    add values to routes req

--- src/ingest_gtfs_schedule/main.rs
+++ src/ingest_gtfs_schedule/main.rs
@@ -398,6 +398,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 continuous_pickup,
                 continuous_drop_off
             )
+            VALUES (
+                $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13
+            )
             ",
                     &[
                         &route_id,
