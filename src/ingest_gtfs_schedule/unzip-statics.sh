795b614746332df327767599990736f174d2da63
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-08-08 23:33:17 -0700

    save changes

--- src/ingest_gtfs_schedule/unzip-statics.sh
+++ src/ingest_gtfs_schedule/unzip-statics.sh
@@ -19,8 +19,43 @@ for zip_file in *.zip; do
     fi
 
     # Unzip the file into the destination folder
-    unzip "$zip_file" -d "../$destination_folder"
+    unzip -f "$zip_file" -d "../$destination_folder"
     echo "Unzipped"
 done
 
 echo "Unzipping complete!"
+
+# we also need to change the permissions bug
+# example is f-kauai 
+
+cd ..
+cd "$destination_dir"
+
+echo "finding and fixing read permissions"
+
+subdirectories=$(find -type d)
+
+# Loop through each subdirectory
+for subdirectory in $subdirectories; do
+
+  # Find all files in the subdirectory
+  files=$(find $subdirectory -type f)
+
+  # Loop through each file
+  for file in $files; do
+
+    # Check if the file has read permissions
+    if [ ! -r $file ]; then
+
+      # Add read permissions to the file
+      
+      echo "-----------------"
+      echo "$subdirectory"
+      echo "fixing missing read perms for $file"
+      chmod +r $subdirectory/$file
+
+    fi
+
+  done
+
+done
