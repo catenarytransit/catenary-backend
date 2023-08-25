e7e61e09f79d5e0ea810438e33fa9949ea32397c
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-08-25 15:05:29 -0700

    add get trip

--- readme.md
+++ readme.md
@@ -9,3 +9,9 @@ sudo apt install protobuf-compiler build-essential gcc pkg-config libssl-dev pos
 ```bash
 cargo run --bin schedule_ingest -- --postgres "host=localhost user=postgres password=correcthorsebatterystaple"
 ```
+
+Example endpoints
+
+`http://localhost:5401/getroutesperagency?feed_id=f-9mu-orangecountytransportationauthority`
+
+`http://localhost:5401/gettrip?feed_id=f-9mu-orangecountytransportationauthority&trip_id=10995882`
--- src/http-server.rs
+++ src/http-server.rs
@@ -95,6 +95,81 @@ struct RouteOutPostgres {
     shapes_list: Vec<String>,
 }
 
+#[derive(serde::Serialize)]
+struct TripPostgres {
+    trip_id: String,
+    onestop_feed_id: String,
+    route_id: String,
+    service_id: String,
+    trip_headsign: Option<String>,
+    trip_short_name: Option<String>,
+    direction_id: Option<i32>,
+    block_id: Option<String>,
+    shape_id: Option<String>,
+    wheelchair_accessible: Option<i32>,
+    bikes_allowed: Option<i32>,
+}
+
+#[actix_web::get("/gettrip")]
+pub async fn gettrip(pool: web::Data<bb8::Pool<bb8_postgres::PostgresConnectionManager<NoTls>>>, req: HttpRequest) -> impl Responder {
+    let mut client = pool.get().await;
+
+    if client.is_ok() {
+    let mut client = client.unwrap();
+    let query_str = req.query_string(); // "name=ferret"
+    let qs = QString::from(query_str);
+    let req_feed_id = qs.get("feed_id").unwrap(); 
+    let trip_id = qs.get("trip_id").unwrap();
+        
+        let postgresresult = client.query("SELECT trip_id, onestop_feed_id, 
+        route_id, service_id, trip_headsign, trip_short_name, direction_id, 
+        block_id, shape_id, wheelchair_accessible, bikes_allowed FROM gtfs.trips
+         WHERE onestop_feed_id = $1 AND trip_id = $2;", &[
+            &req_feed_id,
+            &trip_id
+         ]).await;
+    
+         match postgresresult {
+            Ok(postgresresult) => {
+                let mut result: Vec<TripPostgres> = Vec::new();
+                for row in postgresresult {
+                    result.push(TripPostgres {
+                        trip_id: row.get(0),
+                        onestop_feed_id: row.get(1),
+                        route_id: row.get(2),
+                        service_id: row.get(3),
+                        trip_headsign: row.get(4),
+                        trip_short_name: row.get(5),
+                        direction_id: row.get(6),
+                        block_id: row.get(7),
+                        shape_id: row.get(8),
+                        wheelchair_accessible: row.get(9),
+                        bikes_allowed: row.get(10),
+                    });
+                }
+    
+                let json_string = to_string_pretty(&json!(result)).unwrap();
+    
+                HttpResponse::Ok()
+                    .insert_header(("Content-Type", "application/json"))
+                    .body(json_string)
+            },
+            Err(e) => {
+                println!("{:?}", e);
+                println!("No results from postgres");
+    
+                HttpResponse::InternalServerError()
+                    .insert_header(("Content-Type", "text/plain"))
+                    .body("Postgres Error")
+            }
+         }
+    } else {
+        HttpResponse::InternalServerError()
+        .insert_header(("Content-Type", "text/plain"))
+        .body("Couldn't connect to pool")
+    }
+}
+
 #[actix_web::get("/getroutesperagency")]
 pub async fn getroutesperagency(pool: web::Data<bb8::Pool<bb8_postgres::PostgresConnectionManager<NoTls>>>, req: HttpRequest) -> impl Responder {
     let mut client = pool.get().await;
@@ -160,6 +235,8 @@ pub async fn getroutesperagency(pool: web::Data<bb8::Pool<bb8_postgres::Postgres
     }
 }
 
+
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let postgresstring = arguments::parse(std::env::args())
@@ -194,6 +271,7 @@ let pool  = bb8::Pool::builder().build(manager).await.unwrap();
             .app_data(actix_web::web::Data::new(pool.clone()))
             .route("/", web::get().to(index))
             .service(getroutesperagency)
+            .service(gettrip)
             .route("/getfeeds", web::get().to(getfeeds))
     })
     .workers(4);
