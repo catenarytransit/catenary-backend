7766869739d22d7641bb44666de4f603dea1e790
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-09-03 15:56:47 -0700

    switch key

--- src/ingest_gtfs_schedule/main.rs
+++ src/ingest_gtfs_schedule/main.rs
@@ -906,7 +906,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         gtfs_static_feeds text[],
              */
             client.query("INSERT INTO gtfs.operators (onestop_operator_id, name, gtfs_static_feeds) VALUES ($1, $2, $3);", &[
-                &operator_id,
+                &operator.onestop_id,
                 &operator.name,
                 &operator_to_feed_hashmap.get(&operator_id).clone().map(|associated_feeds| associated_feeds.iter().map(|associated_feed| associated_feed.feed_onestop_id.clone().unwrap_or_else(
                     || "".to_string()
