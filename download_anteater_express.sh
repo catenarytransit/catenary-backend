8131c9871c2c78d79646fc507292c850e327ae55
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-09-23 05:44:08 -0700

    im bad at scripts

--- download_anteater_express.sh
+++ download_anteater_express.sh
@@ -5,6 +5,6 @@ if [ ! -d gtfs_uncompressed/f-anteaterexpress/ ]; then
   mkdir gtfs_uncompressed/f-anteaterexpress/
 fi
 cd gtfs_static_zips/
-curl https://raw.githubusercontent.com/CatenaryMaps/zotgtfs/main/f-anteaterexpress.zip > f-anteaterexpress.zip
+sudo curl https://raw.githubusercontent.com/CatenaryMaps/zotgtfs/main/f-anteaterexpress.zip > f-anteaterexpress.zip
 cd ../gtfs_uncompressed/f-anteaterexpress/
 unzip ../../gtfs_static_zips/f-anteaterexpress.zip
