a4318960e944b33f0cd773792596bd1273481d77
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-09-23 05:26:42 -0700

    new download script

--- download_anteater_express.sh
+++ download_anteater_express.sh
@@ -1,2 +1,5 @@
 cd transitland-atlas/feeds
 curl https://raw.githubusercontent.com/CatenaryMaps/zotgtfs/main/anteaterexpress.dmfr.json > anteaterexpress.dmfr.json
+cd ../..
+cd anteater_gtfs/
+zip -r ../anteater_gtfs.zip *
