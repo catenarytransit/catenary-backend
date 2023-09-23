5cf8428fbde6ef0fa4713e1abd29d87d191684c2
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-09-23 05:27:49 -0700

    anteater express gang

--- download_anteater_express.sh
+++ download_anteater_express.sh
@@ -1,5 +1,5 @@
 cd transitland-atlas/feeds
 curl https://raw.githubusercontent.com/CatenaryMaps/zotgtfs/main/anteaterexpress.dmfr.json > anteaterexpress.dmfr.json
 cd ../..
-cd gtfs_uncompressed/anteater_gtfs/
+cd gtfs_uncompressed/f-anteaterexpress/
 zip -r ../anteater_gtfs.zip *
