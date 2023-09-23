03091cd40236b2d7fec80704f9f518fae5a8b287
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-09-23 05:41:12 -0700

    remove typo

--- download_anteater_express.sh
+++ download_anteater_express.sh
@@ -4,7 +4,7 @@ cd ../..
 if [ ! -d gtfs_uncompressed/f-anteaterexpress/ ]; then
   mkdir gtfs_uncompressed/f-anteaterexpress/
 fi
-cd gtfs_static_zips/f-anteaterexpress.zip
+cd gtfs_static_zips/
 curl https://raw.githubusercontent.com/CatenaryMaps/zotgtfs/main/f-anteaterexpress.zip > f-anteaterexpress.zip
 cd ../../gtfs_uncompressed/f-anteaterexpress/
 unzip ../../gtfs_static_zips/f-anteaterexpress.zip
