301c91d0a2ba29c473adcd2d92ae43b2600cdc3d
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-09-24 01:06:44 -0700

    new antex script

--- download_anteater_express.sh
+++ download_anteater_express.sh
@@ -5,6 +5,8 @@ if [ ! -d gtfs_uncompressed/f-anteaterexpress/ ]; then
   mkdir gtfs_uncompressed/f-anteaterexpress/
 fi
 cd gtfs_static_zips/
+rm -rf f-anteaterexpress.zip
 sudo curl https://raw.githubusercontent.com/CatenaryMaps/zotgtfs/main/f-anteaterexpress.zip > f-anteaterexpress.zip
 cd ../gtfs_uncompressed/f-anteaterexpress/
+rm -rf *
 unzip ../../gtfs_static_zips/f-anteaterexpress.zip
