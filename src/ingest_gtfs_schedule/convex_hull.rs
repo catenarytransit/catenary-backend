bd6585d3ca9b062f32f6b5c96c6c42769fe1aed8
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-10-03 14:08:50 -0700

    add type to convex

--- src/ingest_gtfs_schedule/convex_hull.rs
+++ src/ingest_gtfs_schedule/convex_hull.rs
@@ -3,9 +3,10 @@ use std::cmp::Ordering::Equal;
 use geo::LineString;
 use geo::Coord;
 use geo::coord;
+use geo::ConvexHull;
 
 //graham scan
-pub fn convex_hull(input: &Vec<(f64, f64)>) {
+pub fn convex_hull(input: &Vec<(f64, f64)>) -> geo::Polygon {
     let pnts = input
     .into_iter()
     .map(|xi| coord! {x: xi.0, y: xi.1})
