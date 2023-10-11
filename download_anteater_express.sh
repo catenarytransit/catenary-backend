f3706010c50dbd325afb1fd7039bf75fee0fe205
lolpro11 <89674666+lolpro11@users.noreply.github.com>
2023-10-11 01:28:45 -0700

    Update download_anteater_express.sh

--- download_anteater_express.sh
+++ download_anteater_express.sh
@@ -3,6 +3,6 @@ if [ ! -d gtfs_uncompressed/f-anteaterexpress/ ]; then
   mkdir gtfs_uncompressed/f-anteaterexpress/
 fi
 rm -rf ./gtfs_static_zips/f-anteaterexpress.zip
-sudo curl https://raw.githubusercontent.com/CatenaryMaps/zotgtfs/main/f-anteaterexpress.zip > ./gtfs_static_zips/f-anteaterexpress.zip
+curl https://raw.githubusercontent.com/CatenaryMaps/zotgtfs/main/f-anteaterexpress.zip > ./gtfs_static_zips/f-anteaterexpress.zip
 rm -rf ./gtfs_uncompressed/f-anteaterexpress/*
 unzip ../../gtfs_static_zips/f-anteaterexpress.zip
