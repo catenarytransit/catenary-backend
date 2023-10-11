665d1119e4d65d53a405887dfaf5e7a69227f1a7
lolpro11 <89674666+lolpro11@users.noreply.github.com>
2023-10-11 01:31:31 -0700

    Update download_anteater_express.sh

--- download_anteater_express.sh
+++ download_anteater_express.sh
@@ -5,4 +5,4 @@ fi
 rm -rf ./gtfs_static_zips/f-anteaterexpress.zip
 curl https://raw.githubusercontent.com/CatenaryMaps/zotgtfs/main/f-anteaterexpress.zip > ./gtfs_static_zips/f-anteaterexpress.zip
 rm -rf ./gtfs_uncompressed/f-anteaterexpress/*
-unzip -d ./gtfs_uncompressed/ ./gtfs_static_zips/f-anteaterexpress.zip
+unzip -d ./gtfs_uncompressed/f-anteaterexpress/ ./gtfs_static_zips/f-anteaterexpress.zip
