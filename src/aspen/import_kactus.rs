70f973a05f012c30fcba48bdeefbcc132a2fab6f
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-11-03 03:41:10 -0700

    decode gtfs

--- src/aspen/import_kactus.rs
+++ src/aspen/import_kactus.rs
@@ -1,3 +1,8 @@
 pub async fn new_rt_kactus(realtime_feed_id: String, data: Vec<u8>) -> bool {
+
+    //decode Vec<u8> into gtfs_rt::FeedMessage
+
+    let decoded_gtfs = kactus::parse_protobuf_message(data.as_slice());
+
     true
 }
