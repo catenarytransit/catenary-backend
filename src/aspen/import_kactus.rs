3c77f7995c83d04905665ff69c6f9833bd6d58ac
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-11-03 03:28:13 -0700

    rename and move functions around

---
+++ src/aspen/import_kactus.rs
@@ -0,0 +1,3 @@
+pub async fn new_rt_kactus(realtime_feed_id: String, data: Vec<u8>) -> bool {
+    true
+}
--- src/aspen/lib.rs
+++ src/aspen/lib.rs
@@ -4,7 +4,10 @@ use tracing_subscriber::{fmt::format::FmtSpan, prelude::*};
 /// This is the service definition. It looks a lot like a trait definition.
 /// It defines one RPC, hello, which takes one arg, name, and returns a String.
 #[tarpc::service]
-pub trait World {
+pub trait AspenRpc {
     /// Returns a greeting for name.
     async fn hello(name: String) -> String;
+
+    //maybesend gtfs rt? 
+    async fn new_rt_kactus(realtime_feed_id: String, data: Vec<u8>) -> bool;
 }
