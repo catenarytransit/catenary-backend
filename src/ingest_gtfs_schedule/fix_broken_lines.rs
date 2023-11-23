72924d92b65622d0d511a0179d82adba26561cec
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-11-22 20:20:04 -0800

    rotating calipers algorithm to fix madrid data

--- src/ingest_gtfs_schedule/fix_broken_lines.rs
+++ src/ingest_gtfs_schedule/fix_broken_lines.rs
@@ -1,10 +1,49 @@
+use geo::CoordsIter;
+use geo_types::Point;
 use postgis::ewkb;
 extern crate time;
 extern crate travelling_salesman;
 use geo::HaversineLength;
 use geo::ConvexHull;
-use geo::{line_string, polygon};
+use geo::{line_string, polygon,GeodesicDistance};
 use geo_postgis::{FromPostgis, ToPostgis};
+use geo::EuclideanDistance;
+use geo::HaversineDistance;
+
+use std::f64::consts::PI;
+
+fn rotating_calipers_diameter(polygon: &Vec<geo::Point>) -> f64 {
+    let n = polygon.len();
+
+    let mut k = 1;
+
+    while abs_area(polygon[n-1], polygon[0], polygon[(k+1)%n]) >
+    abs_area(polygon[n-1], polygon[0], polygon[k]) {
+        k = k + 1;
+    }
+
+    let k = k;
+
+    let mut res = 0.;
+    let mut j = k;
+    
+    for i in 0..(k+1) {
+        while abs_area(
+            polygon[i],
+            polygon[(i+1) % n],
+            polygon[(j+1)%n]
+        ) >
+        abs_area(polygon[i], polygon[(i+1)%n], polygon[j])
+        {
+            res = f64::max(res, polygon[i].haversine_distance(&polygon[(j+1)%n]));
+            j = (j+1) % n;
+        }
+
+        res = f64::max(res, polygon[i].haversine_distance(&polygon[j]))
+    }
+
+    res
+}
 
 pub fn fix_broken_lines(
     linestring: ewkb::LineStringT<ewkb::Point>,
@@ -17,8 +56,43 @@ pub fn fix_broken_lines(
 
         println!("{}", length);
 
-        let convex_hulled = geo_typed.convex_hull();
+        let convex_hull = geo_typed.convex_hull();
+        
+        let convex_hull_points = convex_hull.exterior().clone().into_points();
+
+        let farthest_points_distance = match convex_hull_points.len() {
+            0 => 0.,
+            1 => 0.,
+            2 => convex_hull_points[0].geodesic_distance(&convex_hull_points[1]),
+            _ => {
+                rotating_calipers_diameter(&convex_hull_points)
+            }
+        };
+
+        if length > farthest_points_distance * 100. && length > 1000. {
+            println!("Performing travelling salesman on line with {} point distance and {} line distance", farthest_points_distance, length);
+            
+            let points = geo_typed.points().map(|point| (point.x(), point.y())).collect::<Vec<(f64,f64)>>();
+
+            let tour = travelling_salesman::simulated_annealing::solve(
+                &points,
+                time::Duration::milliseconds(5000),
+              );
+            
+            let result:geo::LineString = tour.route.iter().map(|index| 
+                 points[*index]
+            ).collect::<Vec<(f64,f64)>>().into();
+
+            return result.to_postgis_wgs84();
+        }
     }
 
     linestring
 }
+
+
+fn abs_area(p: geo::Point, q : geo::Point, r: geo::Point) -> f64 {
+    return (
+      p.x() * q.y() + q.x() * r.y() + r.x() * p.y() - (p.y() * q.x() + q.y() * r.x() + r.y() * p.x())
+    ).abs();
+  }
