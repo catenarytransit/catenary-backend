db80ab691c169294fcfe55ab202ff850f56bceaf
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-11-24 11:36:28 -0800

    new functions

--- src/ingest_gtfs_schedule/shape_functions.rs
+++ src/ingest_gtfs_schedule/shape_functions.rs
@@ -121,4 +121,50 @@ pub async fn render_vector_tile_functions(client: &tokio_postgres::Client, schem
     END
     $$ LANGUAGE plpgsql IMMUTABLE STRICT PARALLEL SAFE;
     ").as_str()).await.unwrap();
+
+    client.batch_execute(format!("
+    CREATE OR REPLACE
+    FUNCTION {schemaname}.stationfeatures(z integer, x integer, y integer)
+    RETURNS bytea AS $$
+    DECLARE
+    mvt bytea;
+    BEGIN
+    SELECT INTO mvt ST_AsMVT(tile, 'stationfeatures', 4096, 'geom') FROM (
+    SELECT
+    ST_AsMVTGeom(
+        ST_Transform(point, 3857),
+        ST_TileEnvelope(z, x, y),
+        4096, 64, true) AS geom,
+        onestop_feed_id, name, code, gtfs_desc, location_type, parent_station, zone_id, url, timezone, wheelchair_boarding, level_id, platform_code, routes, route_types, children_ids, children_route_types
+    FROM gtfs.stops
+    WHERE (point && ST_Transform(ST_TileEnvelope(z, x, y), 4326)) AND location_type=2
+    ) as tile WHERE geom IS NOT NULL;
+
+    RETURN mvt;
+    END
+    $$ LANGUAGE plpgsql IMMUTABLE STRICT PARALLEL SAFE;
+    ").as_str()).await.unwrap();
+
+    client.batch_execute(format!("
+    CREATE OR REPLACE
+    FUNCTION {schemaname}.busstops(z integer, x integer, y integer)
+    RETURNS bytea AS $$
+    DECLARE
+    mvt bytea;
+    BEGIN
+    SELECT INTO mvt ST_AsMVT(tile, 'busstops', 4096, 'geom') FROM (
+    SELECT
+    ST_AsMVTGeom(
+        ST_Transform(point, 3857),
+        ST_TileEnvelope(z, x, y),
+        4096, 64, true) AS geom,
+        onestop_feed_id, name, code, gtfs_desc, location_type, parent_station, zone_id, url, timezone, wheelchair_boarding, level_id, platform_code, routes, route_types, children_ids, children_route_types
+    FROM gtfs.stops
+    WHERE (point && ST_Transform(ST_TileEnvelope(z, x, y), 4326)) AND  (3 = ANY(route_types) OR 11 = ANY(route_types))
+    ) as tile WHERE geom IS NOT NULL;
+
+    RETURN mvt;
+    END
+    $$ LANGUAGE plpgsql IMMUTABLE STRICT PARALLEL SAFE;
+    ").as_str()).await.unwrap();
 }
