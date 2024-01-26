9ad2b4556522637aeb73b3b6578e097a28586116
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-01-26 00:37:19 -0800

    Move metadata fetch to seperate file
    Call me Linus Torvalds the way I'm maintaining this massive codebase

--- src/next_db/import.rs
+++ src/next_db/import.rs
@@ -9,8 +9,12 @@ use sqlx::query;
 use sqlx::{Connection, PgConnection, PgPool, Postgres};
 use std::time::Duration;
 mod database;
+use std::collections::HashSet;
+mod dmfr;
+use std::sync::Arc;
 
-mod transitland_import;
+mod get_feeds_meta;
+mod transitland_download;
 
 #[tokio::main]
 async fn main() {
@@ -24,5 +28,20 @@ async fn main() {
     //migrate database
     let _ = database::check_for_migrations().await;
 
-    let eligible_feeds = transitland_import::download_return_eligible_feeds(&pool).await;
+    let transitland_metadata: Arc<get_feeds_meta::TransitlandMetadata> = Arc::from(get_feeds_meta::generate_transitland_metadata());
+
+    let eligible_feeds = transitland_download::download_return_eligible_feeds(transitland_metadata.clone(), &pool).await;
+
+    let feeds_to_discard: HashSet<&str> = HashSet::from_iter(vec![
+        "f-9q8y-sfmta",
+        "f-9qc-westcat~ca~us",
+        "f-9q9-actransit",
+        "f-9q9-vta",
+        "f-9q8yy-missionbaytma~ca~us",
+        "f-9qbb-marintransit",
+        "f-9q8-samtrans",
+        "f-9q9-bart",
+        "f-9q9-caltrain",
+        "f-9qc3-riovistadeltabreeze",
+    ]);
 }
