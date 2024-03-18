cb01c5bce6623e1ef5d4e9a85418f7c38e15c2b5
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-03-18 14:36:07 -0700

    Add comments explaining new kactus architecture

---
+++ src/kactus/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // infinite runtime with worker threads and a leader-candidate thread that attempts to be the leader
+    // Uses zookeeper to elect the leader and assign feeds to insert
+    // Service discovery via zookeeper
+
+    // if a node drops out, ingestion will be automatically reassigned to the other nodes
+
+    //hands off data to aspen to do additional cleanup and processing, Aspen will perform association with the GTFS schedule data + update dynamic graphs for routing and map representation,
+    //aspen will also forward critical alerts to users
+}
