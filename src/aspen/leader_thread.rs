34a91f726de1b89ba9b24940047cbc6a03a137a1
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-04-23 21:35:38 -0700

    print loop

--- src/aspen/leader_thread.rs
+++ src/aspen/leader_thread.rs
@@ -35,6 +35,9 @@ pub async fn aspen_leader_thread(
     println!("Leader connected to postgres");
 
     loop {
+
+        println!("loop");
+
         let (zk, default_watcher) = ZooKeeper::connect(&"127.0.0.1:2181".parse().unwrap())
             .await
             .unwrap();
