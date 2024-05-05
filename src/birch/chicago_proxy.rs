7ca6571eaabd35c2db03e60ebd182c0d1e31962d
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-05-05 16:09:39 -0700

    Extract using web query for cta arrivals

--- src/birch/chicago_proxy.rs
+++ src/birch/chicago_proxy.rs
@@ -1,9 +1,13 @@
-use actix_web::{HttpRequest, HttpResponse, Responder};
-use tungstenite::http::request;
+pub struct TTArrivalsQuery {
+    map_id: String
+ }
+
+use actix_web::{HttpRequest, HttpResponse, Responder, web};
 #[actix_web::get("/cta_ttarrivals_proxy")]
-pub async fn ttarrivals_proxy(req: HttpRequest) -> impl Responder {
+pub async fn ttarrivals_proxy(req: HttpRequest, query: web::Query<TTArrivalsQuery>) -> impl Responder {
     //get query param mapid
-    let map_id = req.match_info().query("mapid");
+
+    let map_id = &query.map_id;
 
     //https://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=e325bc1ce4ad4ce0a3ad0830739c4993&mapid=40380&outputType=JSON
     let url = format!("https://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=e325bc1ce4ad4ce0a3ad0830739c4993&mapid={}&outputType=JSON", map_id);
