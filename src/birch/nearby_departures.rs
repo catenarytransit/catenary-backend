4f9314272445387777500ad9dd8839c1ad5b659d
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-05-07 17:21:36 -0700

    add comment about serde json

--- src/birch/nearby_departures.rs
+++ src/birch/nearby_departures.rs
@@ -31,6 +31,7 @@ pub async fn nearby_from_coords(req: HttpRequest, query: Query<NearbyFromCoords>
 
 #[derive(Deserialize, Clone, Debug)]
 struct NearbyFromStops {
+    //serialise and deserialise using serde_json
     stops: Vec<String>,
 }
 
