702954e8ba0092b8945ac8e87b636b411cb9042c
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-06-12 15:21:12 -0700

    Add stop to stop shape migration

--- migrations/2024-06-12-071029_allow_stop_to_stop_shape/down.sql
+++ migrations/2024-06-12-071029_allow_stop_to_stop_shape/down.sql
@@ -1,1 +1,3 @@
 -- This file should undo anything in `up.sql`
+ALTER TABLE gtfs.direction_pattern_meta DROP COLUMN route_id;
+ALTER TABLE gtfs.shapes DROP COLUMN stop_to_stop_generated;
--- migrations/2024-06-12-071029_allow_stop_to_stop_shape/up.sql
+++ migrations/2024-06-12-071029_allow_stop_to_stop_shape/up.sql
@@ -1,1 +1,3 @@
 -- Your SQL goes here
+ALTER TABLE gtfs.direction_pattern_meta ADD route_id text;
+ALTER TABLE gtfs.shapes ADD stop_to_stop_generated boolean;
