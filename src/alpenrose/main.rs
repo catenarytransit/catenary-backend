42d93fdb1900fa9cb06d95d656fa2d3cd92ee2e1
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-08-04 01:35:13 -0700

    shorten the alpenrose lease to 10s, rapidly renew the lease

--- src/alpenrose/main.rs
+++ src/alpenrose/main.rs
@@ -126,8 +126,8 @@ async fn main() -> Result<(), Box<dyn Error + Sync + Send>> {
 
     let make_lease = etcd
         .lease_grant(
-            //60 seconds
-            60,
+            //10 seconds
+            10,
             Some(etcd_client::LeaseGrantOptions::new().with_id(etcd_lease_id)),
         )
         .await?;
