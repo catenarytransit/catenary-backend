3d777a881b9980f20c1cc84e2744ebbd663eb415
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-10-11 22:45:17 -0700

    print read duration

--- src/maple/gtfs_process.rs
+++ src/maple/gtfs_process.rs
@@ -65,6 +65,8 @@ pub async fn gtfs_process_feed(
 
     let gtfs = gtfs_structures::Gtfs::new(path.as_str())?;
 
+    println!("Finished reading GTFS for {}, took {:?}", feed_id, gtfs.read_duration);
+
     // Read Translations.txt, don't fail if it doesn't exist
     let translation_path = format!("{}/{}/translations.txt", gtfs_unzipped_path, feed_id);
     let translation_data = std::fs::read_to_string(translation_path);
