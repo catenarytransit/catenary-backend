c3dad9a12e70d52c12a79c08b1882d34572dd7dc
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-11-15 01:50:05 -0800

    filter shapes as well

--- src/schedule_filtering/mod.rs
+++ src/schedule_filtering/mod.rs
@@ -23,11 +23,17 @@ pub fn include_only_route_types(gtfs: Gtfs, route_types: Vec<gtfs_structures::Ro
 
     let mut keep_stop_ids: BTreeSet<String> = BTreeSet::new();
 
+    let mut keep_shapes: BTreeSet<String> = BTreeSet::new();
+
     for trip_id in &trips_to_keep {
         let trip = gtfs.trips.get(trip_id).unwrap();
 
         for stop_time in &trip.stop_times {
             keep_stop_ids.insert(stop_time.stop.id.clone());
+        }
+
+        if let Some(shape_id) = &trip.shape_id {
+            keep_shapes.insert(shape_id.clone());
         }
     }
 
@@ -51,5 +57,30 @@ pub fn include_only_route_types(gtfs: Gtfs, route_types: Vec<gtfs_structures::Ro
         .filter(|(stop_id, stop)| keep_stop_ids.contains(stop_id))
         .collect();
 
+    gtfs.shapes = gtfs
+        .shapes
+        .into_iter()
+        .filter(|(shape_id, shape)| keep_shapes.contains(shape_id))
+        .collect();
+
     gtfs
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn filter_ttc() {
+        let gtfs = gtfs_structures::Gtfs::from_url_async("https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/7795b45e-e65a-4465-81fc-c36b9dfff169/resource/cfb6b2b8-6191-41e3-bda1-b175c51148cb/download/TTC%20Routes%20and%20Schedules%20Data.zip").await.unwrap();
+    
+       
+        println!("starts with");
+        gtfs.print_stats();
+    
+        let gtfs = include_only_route_types(gtfs, vec![gtfs_structures::RouteType::Subway]);
+    
+        println!("ends with");
+        let final_stats = gtfs.print_stats();
+    }
 }
