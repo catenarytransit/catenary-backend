e9d8542d6112afe23c7da9dbf4c1cd88d74ee2f3
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2025-02-01 16:01:24 -0800

    collect more full text alerts

--- src/custom_alerts/metrolink_alerts.rs
+++ src/custom_alerts/metrolink_alerts.rs
@@ -16,6 +16,11 @@ pub const METROLINK_ALERTS_URL: &str = "https://metrolinktrains.com/advisories/g
 pub const GREETING: &str = "Good morning! We'll be posting your train status updates from 4:00 a.m. to 12:30 a.m. Be safe and have a great day!";
 pub const GREETING_WEEKEND: &str = "Good morning! We'll be posting your train status updates from 6:00 a.m. to 11:00 p.m. Be safe and have a great day!";
 
+pub const REMOVE_DELUSION: &str = "Please review OCTA bus routes connecting Laguna Niguel/Mission Viejo and Oceanside as alternatives.";
+pub const REMOVE_YAP: &str = "We apologize that necessary infrastructure enhancements sometimes result in track closures that we know may be inconvenient for our riders. This work is needed to ensure the safety and reliability of the system.";
+
+pub const ALERT_URL_PREFIX: &str = "https://metrolinktrains.com/news/alert-details-page/?alertId=";
+
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct RawMetrolinkEachRoute {
@@ -140,7 +145,7 @@ pub async fn fetch_alert_page_data(
         .replace(
             " For real-time updates, follow us on Facebook and Twitter (X).",
             "",
-        );
+        ).replace(REMOVE_DELUSION, "").replace(REMOVE_YAP, "");
 
     let start_date_selector = scraper::Selector::parse(".alertsDetail__date--start").unwrap();
     //pick first optionally
@@ -180,7 +185,7 @@ pub async fn fetch_alert_page_data(
         route_ids,
         id: url
             .replace(
-                "https://metrolinktrains.com/news/alert-details-page/?alertId=",
+                ALERT_URL_PREFIX,
                 "",
             )
             .to_string(),
@@ -238,7 +243,7 @@ pub async fn fetch_alerts_from_root_metrolink(
     Ok(alert_details)
 }
 
-pub async fn determine_cause(header: &str, desc: &str) -> i32 {
+pub fn determine_cause(header: &str, desc: &str) -> i32 {
     let header_lower = header.to_lowercase();
 
     if header_lower.contains("construction") {
@@ -268,7 +273,7 @@ pub async fn determine_cause(header: &str, desc: &str) -> i32 {
     return 2;
 }
 
-pub async fn determine_effect(header: &str, desc: &str) -> i32 {
+pub fn determine_effect(header: &str, desc: &str) -> i32 {
     if header.to_lowercase().contains("no train service")
         || header.to_lowercase().contains("no service")
     {
@@ -276,6 +281,10 @@ pub async fn determine_effect(header: &str, desc: &str) -> i32 {
     }
 
     if header.to_lowercase().contains("significant delays") {
+        return 2;
+    }
+
+    if header.to_lowercase().contains("delay") {
         return 2;
     }
 
@@ -291,6 +300,65 @@ pub fn make_en_translated_string(text: String) -> gtfs_realtime::TranslatedStrin
     }
 }
 
+fn page_to_gtfs_rt_alerts(page: RawAlertDetailsPage) -> gtfs_realtime::FeedEntity {
+    let cause = determine_cause(&page.header, &page.description);
+    let effect = determine_effect(&page.header, &page.description);
+
+    let mut route_ids = vec![];
+
+    for route_id in page.route_ids {
+        let route_id = route_id.to_string();
+        route_ids.push(route_id);
+    }
+
+    let start_date = page.start_date;
+    let end_date = page.end_date;
+
+    //convert start_date to 00:00:00 and end_date to 23:59:59, both in Los Angeles time
+
+    let start_dt = start_date
+        .map(|x| x.and_hms_opt(0, 0, 0))
+        .flatten()
+        .map(|x| Pacific.from_local_datetime(&x).single())
+        .flatten();
+
+    let end_dt = end_date
+        .map(|x| x.and_hms_opt(23, 59, 59))
+        .flatten()
+        .map(|x| Pacific.from_local_datetime(&x).single())
+        .flatten();
+
+    //make timestamps for both start and end
+
+    let start_timestamp: Option<u64> = start_dt.map(|x| x.timestamp() as u64);
+    let end_timestamp: Option<u64> = end_dt.map(|x| x.timestamp() as u64);
+
+    gtfs_realtime::FeedEntity {
+        id: format!("alert-{}", page.id),
+        is_deleted: None,
+        alert: Some(gtfs_realtime::Alert {
+            cause: Some(cause),
+            effect: Some(effect),
+            url: Some(make_en_translated_string(page.url)),
+            header_text: Some(make_en_translated_string(page.header)),
+            description_text: Some(make_en_translated_string(page.description)),
+            active_period: vec![gtfs_realtime::TimeRange {
+                start: start_timestamp,
+                end: end_timestamp,
+            }],
+            informed_entity: route_ids
+                .into_iter()
+                .map(|route_id| gtfs_realtime::EntitySelector {
+                    route_id: Some(route_id),
+                    ..Default::default()
+                })
+                .collect(),
+            ..Default::default()
+        }),
+        ..Default::default()
+    }
+}
+
 pub async fn gtfs_rt_alerts_from_metrolink_website(
     client: &reqwest::Client,
 ) -> Result<Vec<gtfs_realtime::FeedEntity>, Box<dyn Error + Send + Sync>> {
@@ -299,66 +367,14 @@ pub async fn gtfs_rt_alerts_from_metrolink_website(
     let raw_from_homepage = fetch_alerts_from_root_metrolink(client).await?;
 
     //conversion to gtfs rt
-    for raw_homepage_alert in raw_from_homepage {
-        let cause =
-            determine_cause(&raw_homepage_alert.header, &raw_homepage_alert.description).await;
-        let effect =
-            determine_effect(&raw_homepage_alert.header, &raw_homepage_alert.description).await;
+    let mut finished_id_list = raw_from_homepage.iter().map(|x| x.id.clone()).collect_vec();
 
-        let mut route_ids = vec![];
+    let gtfs_rt_entities_from_homepage = raw_from_homepage
+        .into_iter()
+        .map(page_to_gtfs_rt_alerts)
+        .collect::<Vec<gtfs_realtime::FeedEntity>>();
 
-        for route_id in raw_homepage_alert.route_ids {
-            let route_id = route_id.to_string();
-            route_ids.push(route_id);
-        }
-
-        let start_date = raw_homepage_alert.start_date;
-        let end_date = raw_homepage_alert.end_date;
-
-        //convert start_date to 00:00:00 and end_date to 23:59:59, both in Los Angeles time
-
-        let start_dt = start_date
-            .map(|x| x.and_hms_opt(0, 0, 0))
-            .flatten()
-            .map(|x| Pacific.from_local_datetime(&x).single())
-            .flatten();
-
-        let end_dt = end_date
-            .map(|x| x.and_hms_opt(23, 59, 59))
-            .flatten()
-            .map(|x| Pacific.from_local_datetime(&x).single())
-            .flatten();
-
-        //make timestamps for both start and end
-
-        let start_timestamp: Option<u64> = start_dt.map(|x| x.timestamp() as u64);
-        let end_timestamp: Option<u64> = end_dt.map(|x| x.timestamp() as u64);
-
-        entities.push(gtfs_realtime::FeedEntity {
-            id: format!("alert-{}", raw_homepage_alert.id),
-            is_deleted: None,
-            alert: Some(gtfs_realtime::Alert {
-                cause: Some(cause),
-                effect: Some(effect),
-                url: Some(make_en_translated_string(raw_homepage_alert.url)),
-                header_text: Some(make_en_translated_string(raw_homepage_alert.header)),
-                description_text: Some(make_en_translated_string(raw_homepage_alert.description)),
-                active_period: vec![gtfs_realtime::TimeRange {
-                    start: start_timestamp,
-                    end: end_timestamp,
-                }],
-                informed_entity: route_ids
-                    .into_iter()
-                    .map(|route_id| gtfs_realtime::EntitySelector {
-                        route_id: Some(route_id),
-                        ..Default::default()
-                    })
-                    .collect(),
-                ..Default::default()
-            }),
-            ..Default::default()
-        });
-    }
+    entities.extend(gtfs_rt_entities_from_homepage);
 
     let body_of_alerts = client
         .get(METROLINK_ALERTS_URL)
@@ -390,6 +406,26 @@ pub async fn gtfs_rt_alerts_from_metrolink_website(
         })
         .collect_vec();
 
+    let attempt_to_fetch_more_full_alerts = raw_data.iter().map(|each_route| each_route.service_advisories.map(|x| x.id)).flatten()
+    //list of alert ids
+    // only ids over the number 1000000 have reported been containing text
+    .filter(|id_num| id_num > 1000000)
+    .filter(|id_num| !finished_id_list.contains(&id_num.to_string()))
+    .map(|id_num| {
+        format!("{}{}", ALERT_URL_PREFIX, id_num)
+    })
+    .map(|url| fetch_alert_page_data(url.as_str(), client))
+    .filter_map(|x| x.ok())
+    .collect::<Vec<RawAlertDetailsPage>>();
+
+    let gtfs_rt_entities_from_advisories_page_full_text = attempt_to_fetch_more_full_alerts
+        .into_iter()
+        .map(page_to_gtfs_rt_alerts)
+        .collect::<Vec<gtfs_realtime::FeedEntity>>();
+
+    entities.extend(gtfs_rt_entities_from_advisories_page_full_text);
+    finished_id_list.extend(attempt_to_fetch_more_full_alerts.iter().map(|x| x.id.clone()));
+
     let gtfs_rt_entities_from_advisories_page = raw_data
         .into_iter()
         .map(|each_route| {
@@ -400,6 +436,7 @@ pub async fn gtfs_rt_alerts_from_metrolink_website(
                     each_route
                         .service_advisories
                         .into_iter()
+                        .filter(|service_advisory| !finished_id_list.contains(&service_advisory.id.to_string()))
                         .map(move |service_advisory| FeedEntity {
                             id: format!("alert-{}", service_advisory.id),
                             is_deleted: None,
