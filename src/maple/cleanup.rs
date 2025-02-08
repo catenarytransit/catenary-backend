39b8d95fa6a38e0f4b6a4404f7dab27724c4635d
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2025-02-07 22:33:27 -0800

    correct deletion logic

--- src/maple/cleanup.rs
+++ src/maple/cleanup.rs
@@ -176,16 +176,16 @@ pub async fn delete_attempt_objects(
     .execute(conn)
     .await?;
 
-    //delete ingested static_download_attempts
+    let _ = diesel::update(
+        catenary::schema::gtfs::ingested_static::dsl::ingested_static
+            .filter(catenary::schema::gtfs::ingested_static::dsl::onestop_feed_id.eq(&feed_id))
+             .filter(catenary::schema::gtfs::ingested_static::dsl::attempt_id.eq(&attempt_id))
+    )
+            .set(catenary::schema::gtfs::ingested_static::dsl::deleted.eq(true))
     
+    .execute(conn)
+    .await?;
 
-    use catenary::schema::gtfs::static_download_attempts;
-    use catenary::schema::gtfs::static_download_attempts::dsl::static_download_attempts as static_download_attempts_table;
-
-    let _ = diesel::delete(
-        static_download_attempts_table
-        .filter(static_download_attempts::dsl::onestop_feed_id.eq(&feed_id).and(static_download_attempts::dsl::attempt_id.eq(&attempt_id)))
-    ).execute(conn).await?;
 
     Ok(())
 }
@@ -298,17 +298,14 @@ pub async fn wipe_whole_feed(
     .execute(conn)
     .await?;
 
-    //delete ingested static_download_attempts
-    /*
-
-    use catenary::schema::gtfs::static_download_attempts;
-    use catenary::schema::gtfs::static_download_attempts::dsl::static_download_attempts as static_download_attempts_table;
-
-    let _ = diesel::delete(
-        static_download_attempts_table
-        .filter(static_download_attempts::dsl::onestop_feed_id.eq(&feed_id).and(static_download_attempts::dsl::attempt_id.eq(&attempt_id)))
-    ).execute(conn).await;
-     */
+    let _ = diesel::update(
+        catenary::schema::gtfs::ingested_static::dsl::ingested_static
+            .filter(catenary::schema::gtfs::ingested_static::dsl::onestop_feed_id.eq(&feed_id))
+    )
+            .set(catenary::schema::gtfs::ingested_static::dsl::deleted.eq(true))
+    
+    .execute(conn)
+    .await?;
 
     Ok(())
 }
