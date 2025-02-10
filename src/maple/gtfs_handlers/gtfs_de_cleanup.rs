91fbd77259fed9c49ef3cd0b56a53208da3c5d8b
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2025-02-10 15:02:19 -0800

    first version of deutschland filter

--- src/maple/gtfs_handlers/gtfs_de_cleanup.rs
+++ src/maple/gtfs_handlers/gtfs_de_cleanup.rs
@@ -1,9 +1,16 @@
 use gtfs_structures::Gtfs;
-use std::collections::HashMap;
+use std::collections::{BTreeSet, HashMap};
 
 pub fn gtfs_de_cleanup(gtfs: Gtfs) -> Gtfs {
     let mut gtfs = gtfs;
-    let to_delete_agencies = ["Flixbus", "Stadtwerke München"];
+    let to_delete_agencies = [
+        "Flixbus",
+        "Stadtwerke München",
+        "Berliner Verkehrsbetriebe",
+        "Karlsruher Verkehrsverbund",
+        "Hamburger Verkehrsverbund",
+        "Freiburger Verkehrs AG"
+    ];
 
     let agency_ids_to_remove = to_delete_agencies
         .iter()
@@ -22,7 +29,54 @@ pub fn gtfs_de_cleanup(gtfs: Gtfs) -> Gtfs {
     gtfs.agencies
         .retain(|x| !agency_ids_to_remove.contains(&x.id.as_ref().unwrap()));
 
-    let stops_to_agency_ids: HashMap<String, Vec<String>> = HashMap::new();
+    let mut stops_to_agency_ids: HashMap<String, BTreeSet<String>> = HashMap::new();
+
+    for (trip_id, trip) in gtfs.trips.iter() {
+        let agency_id = gtfs.routes.get(&trip.route_id).unwrap().agency_id.clone();
+
+        if let Some(agency_id) = &agency_id {
+            for stop_time in &trip.stop_times {
+                let stop_id = stop_time.stop.id.clone();
+
+                if agency_ids_to_remove.contains(agency_id) {
+                    continue;
+                }
+
+                if stops_to_agency_ids.contains_key(&stop_id) {
+                    stops_to_agency_ids
+                        .get_mut(&stop_id)
+                        .unwrap()
+                        .insert(agency_id.clone());
+                } else {
+                    let mut agency_ids = BTreeSet::new();
+                    agency_ids.insert(agency_id.clone());
+                    stops_to_agency_ids.insert(stop_id.clone(), agency_ids);
+                }
+            }
+        }
+    }
+
+    gtfs.trips
+        .retain(|trip_id, trip| match gtfs.routes.get(&trip.route_id) {
+            Some(route) => match &route.agency_id {
+                Some(agency_id) => !agency_ids_to_remove.contains(agency_id),
+                None => true,
+            },
+            //route id is not valid, remove trip
+            None => false,
+        });
+
+    gtfs.stops
+        .retain(|stop_id, stop| match stops_to_agency_ids.get(stop_id) {
+            Some(agency_ids) => !agency_ids.is_empty(),
+            None => true,
+        });
+
+    gtfs.routes
+        .retain(|route_id, route| match &route.agency_id {
+            Some(agency_id) => !agency_ids_to_remove.contains(agency_id),
+            None => true,
+        });
 
     gtfs
 }
