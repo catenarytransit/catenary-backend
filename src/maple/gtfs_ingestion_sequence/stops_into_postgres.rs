b4efdef92ca6742907fe93d7249fc32fcf69e879
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2025-02-10 18:31:51 -0800

    make stops insertion faster

--- src/maple/gtfs_ingestion_sequence/stops_into_postgres.rs
+++ src/maple/gtfs_ingestion_sequence/stops_into_postgres.rs
@@ -8,6 +8,7 @@ use catenary::schema::gtfs::stops::dsl::stops as stops_table;
 use diesel_async::RunQueryDsl;
 use std::collections::{HashMap, HashSet};
 use std::sync::Arc;
+use itertools::Itertools;
 use titlecase::titlecase;
 
 pub async fn stops_into_postgres(
@@ -21,124 +22,133 @@ pub async fn stops_into_postgres(
     stop_id_to_children_ids: &HashMap<String, HashSet<String>>,
     stop_id_to_children_route: &HashMap<String, HashSet<i16>>,
 ) -> Result<(), Box<dyn std::error::Error + Sync + Send>> {
-    for (stop_id, stop) in &gtfs.stops {
-        let name: Option<String> = titlecase_process_new(stop.name.as_ref());
-        let display_name: Option<String> = name.as_ref().map(|name| {
-            name.clone()
-                .replace("Station ", "")
-                .replace(" Station", "")
-                .replace(", Bahnhof", "")
-                .replace(" Banhhof", "")
-                .replace("Estación de tren ", "")
-                .replace(" Metrolink", "")
-                .replace("Northbound", "N.B.")
-                .replace("Eastbound", "E.B.")
-                .replace("Southbound", "S.B.")
-                .replace("Westbound", "W.B.")
-                .replace(" (Railway) ", "")
-                .replace(" Light Rail", "")
-        });
+    
+    let conn_pool = arc_conn_pool.as_ref();
+    let conn_pre = conn_pool.get().await;
+    let conn = &mut conn_pre?;
 
-        let stop_pg = catenary::models::Stop {
-            onestop_feed_id: feed_id.to_string(),
-            chateau: chateau_id.to_string(),
-            attempt_id: attempt_id.to_string(),
-            gtfs_id: stop_id.clone(),
-            name,
-            name_translations: None,
-            displayname: display_name,
-            code: match feed_id {
-                "f-amtrak~sanjoaquin" => Some(stop_id.clone()),
-                _ => stop.code.clone(),
-            },
-            gtfs_desc: stop.description.clone(),
-            gtfs_desc_translations: None,
-            location_type: location_type_conversion(&stop.location_type),
-            children_ids: match stop_id_to_children_ids.get(&stop.id) {
-                Some(children_ids) => children_ids
-                    .iter()
-                    .map(|x| Some(x.clone()))
-                    .collect::<Vec<Option<String>>>(),
-                None => vec![],
-            },
-            location_alias: None,
-            hidden: false,
-            parent_station: stop.parent_station.clone(),
-            zone_id: stop.zone_id.clone(),
-            url: stop.url.clone(),
-            point: match stop.latitude.is_some() && stop.longitude.is_some() {
-                true => Some(postgis_diesel::types::Point::new(
-                    stop.longitude.unwrap(),
-                    stop.latitude.unwrap(),
-                    Some(4326),
-                )),
-                false => match stop.parent_station.is_some() {
-                    true => {
-                        let parent_station = gtfs.stops.get(stop.parent_station.as_ref().unwrap());
-                        match parent_station {
-                            Some(parent_station) => match parent_station.latitude.is_some()
-                                && parent_station.longitude.is_some()
-                            {
-                                true => Some(postgis_diesel::types::Point::new(
-                                    parent_station.longitude.unwrap(),
-                                    parent_station.latitude.unwrap(),
-                                    Some(4326),
-                                )),
-                                false => None,
-                            },
-                            None => None,
-                        }
-                    }
-                    false => None,
+    for chunk in &gtfs.stops.iter().chunks(64) {
+        let mut insertable_stops = Vec::new();
+
+        for (stop_id, stop) in chunk {
+            let name: Option<String> = titlecase_process_new(stop.name.as_ref());
+            let display_name: Option<String> = name.as_ref().map(|name| {
+                name.clone()
+                    .replace("Station ", "")
+                    .replace(" Station", "")
+                    .replace(", Bahnhof", "")
+                    .replace(" Banhhof", "")
+                    .replace("Estación de tren ", "")
+                    .replace(" Metrolink", "")
+                    .replace("Northbound", "N.B.")
+                    .replace("Eastbound", "E.B.")
+                    .replace("Southbound", "S.B.")
+                    .replace("Westbound", "W.B.")
+                    .replace(" (Railway) ", "")
+                    .replace(" Light Rail", "")
+            });
+    
+            let stop_pg = catenary::models::Stop {
+                onestop_feed_id: feed_id.to_string(),
+                chateau: chateau_id.to_string(),
+                attempt_id: attempt_id.to_string(),
+                gtfs_id: stop_id.clone(),
+                name,
+                name_translations: None,
+                displayname: display_name,
+                code: match feed_id {
+                    "f-amtrak~sanjoaquin" => Some(stop_id.clone()),
+                    _ => stop.code.clone(),
                 },
-            },
-            timezone: stop.timezone.clone(),
-            level_id: stop.level_id.clone(),
-            station_feature: false,
-            wheelchair_boarding: availability_to_int(&stop.wheelchair_boarding),
-            primary_route_type: match stop_ids_to_route_types.get(&stop.id) {
-                Some(route_types) => {
-                    let route_types = route_types.iter().copied().collect::<Vec<i16>>();
-                    Some(route_types[0])
-                }
-                None => None,
-            },
-            platform_code: stop.platform_code.clone(),
-            routes: match stop_ids_to_route_ids.get(&stop.id) {
-                Some(route_ids) => route_ids
-                    .iter()
-                    .map(|x| Some(x.clone()))
-                    .collect::<Vec<Option<String>>>(),
-                None => vec![],
-            },
-            children_route_types: match stop_id_to_children_route.get(&stop.id) {
-                Some(route_types) => route_types
-                    .iter()
-                    .map(|x| Some(*x))
-                    .collect::<Vec<Option<i16>>>(),
-                None => vec![],
-            },
-            tts_name: stop.tts_name.clone(),
-            tts_name_translations: None,
-            platform_code_translations: None,
-            route_types: match stop_ids_to_route_types.get(&stop.id) {
-                Some(route_types) => route_types
-                    .iter()
-                    .map(|x| Some(*x))
-                    .collect::<Vec<Option<i16>>>(),
-                None => vec![],
-            },
-            allowed_spatial_query: false,
-        };
-
-        let conn_pool = arc_conn_pool.as_ref();
-        let conn_pre = conn_pool.get().await;
-        let conn = &mut conn_pre?;
+                gtfs_desc: stop.description.clone(),
+                gtfs_desc_translations: None,
+                location_type: location_type_conversion(&stop.location_type),
+                children_ids: match stop_id_to_children_ids.get(&stop.id) {
+                    Some(children_ids) => children_ids
+                        .iter()
+                        .map(|x| Some(x.clone()))
+                        .collect::<Vec<Option<String>>>(),
+                    None => vec![],
+                },
+                location_alias: None,
+                hidden: false,
+                parent_station: stop.parent_station.clone(),
+                zone_id: stop.zone_id.clone(),
+                url: stop.url.clone(),
+                point: match stop.latitude.is_some() && stop.longitude.is_some() {
+                    true => Some(postgis_diesel::types::Point::new(
+                        stop.longitude.unwrap(),
+                        stop.latitude.unwrap(),
+                        Some(4326),
+                    )),
+                    false => match stop.parent_station.is_some() {
+                        true => {
+                            let parent_station = gtfs.stops.get(stop.parent_station.as_ref().unwrap());
+                            match parent_station {
+                                Some(parent_station) => match parent_station.latitude.is_some()
+                                    && parent_station.longitude.is_some()
+                                {
+                                    true => Some(postgis_diesel::types::Point::new(
+                                        parent_station.longitude.unwrap(),
+                                        parent_station.latitude.unwrap(),
+                                        Some(4326),
+                                    )),
+                                    false => None,
+                                },
+                                None => None,
+                            }
+                        }
+                        false => None,
+                    },
+                },
+                timezone: stop.timezone.clone(),
+                level_id: stop.level_id.clone(),
+                station_feature: false,
+                wheelchair_boarding: availability_to_int(&stop.wheelchair_boarding),
+                primary_route_type: match stop_ids_to_route_types.get(&stop.id) {
+                    Some(route_types) => {
+                        let route_types = route_types.iter().copied().collect::<Vec<i16>>();
+                        Some(route_types[0])
+                    }
+                    None => None,
+                },
+                platform_code: stop.platform_code.clone(),
+                routes: match stop_ids_to_route_ids.get(&stop.id) {
+                    Some(route_ids) => route_ids
+                        .iter()
+                        .map(|x| Some(x.clone()))
+                        .collect::<Vec<Option<String>>>(),
+                    None => vec![],
+                },
+                children_route_types: match stop_id_to_children_route.get(&stop.id) {
+                    Some(route_types) => route_types
+                        .iter()
+                        .map(|x| Some(*x))
+                        .collect::<Vec<Option<i16>>>(),
+                    None => vec![],
+                },
+                tts_name: stop.tts_name.clone(),
+                tts_name_translations: None,
+                platform_code_translations: None,
+                route_types: match stop_ids_to_route_types.get(&stop.id) {
+                    Some(route_types) => route_types
+                        .iter()
+                        .map(|x| Some(*x))
+                        .collect::<Vec<Option<i16>>>(),
+                    None => vec![],
+                },
+                allowed_spatial_query: false,
+            };
+    
+           
+    
+            insertable_stops.push(stop_pg);
+        }
 
         diesel::insert_into(stops_table)
-            .values(stop_pg)
-            .execute(conn)
-            .await?;
+                .values(insertable_stops)
+                .execute(conn)
+                .await?;
     }
 
     Ok(())
