e4267f838aa249b7b7b00d46b42e124c06880401
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2025-03-02 15:31:35 -0800

    input params for transfer calc fn

--- src/birch/transfer_calc.rs
+++ src/birch/transfer_calc.rs
@@ -1,7 +1,9 @@
 use catenary::models::Route;
 use catenary::models::Stop;
+use catenary::postgres_tools::CatenaryPostgresPool;
 use serde::Serialize;
 use std::collections::HashMap;
+use std::sync::Arc;
 
 #[derive(Debug, Clone, Serialize)]
 pub struct Transfers {
@@ -17,4 +19,10 @@ pub struct IndividualTransferInfo {
     pub distance_metres: f32,
 }
 
-pub async fn transfer_calc() -> () {}
+pub async fn transfer_calc(
+    pool: Arc<CatenaryPostgresPool>,
+    input_stops: Vec<&Stop>,
+    radius_metres: f32,
+) -> () {
+    
+}
