3cc3441000c146721f2d3133bff9aa24302c3661
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2025-03-01 17:42:52 -0800

    add user agent

--- src/alpenrose/single_fetch_time.rs
+++ src/alpenrose/single_fetch_time.rs
@@ -442,6 +442,8 @@ pub fn make_reqwest_for_url(
 
             let mut request = client.get(url);
 
+            request = request.header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0");
+
             if let Some(passwords) = &assignment.passwords {
                 //choose random account to use
                 if !passwords.is_empty() {
