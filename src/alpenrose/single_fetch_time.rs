6b18248ada4ed3623736ff0cf6b92d9517ce83b3
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2025-03-03 17:37:55 -0800

    tell do not cache

--- src/alpenrose/single_fetch_time.rs
+++ src/alpenrose/single_fetch_time.rs
@@ -447,6 +447,11 @@ pub fn make_reqwest_for_url(
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0",
             );
 
+            request = request.header(
+                "Cache-Control",
+                "no-cache"
+            );
+
             if let Some(passwords) = &assignment.passwords {
                 //choose random account to use
                 if !passwords.is_empty() {
