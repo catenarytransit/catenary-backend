aa156559da0f3a3ff56476918bff8fe8896d1e31
Kyler Chin <kyler@catenarymaps.org>
2025-03-16 01:07:08 -0700

    cleanup terrain tiles function

--- src/birch/server.rs
+++ src/birch/server.rs
@@ -66,6 +66,8 @@ mod gtfs_rt_api;
 mod nearby_departures;
 mod route_info;
 use rand::Rng;
+mod terrain_tiles_proxy;
+use terrain_tiles_proxy::proxy_for_maptiler_terrain_tiles;
 
 #[derive(Clone, Debug)]
 struct ChateauCache {
@@ -774,117 +776,6 @@ pub async fn proxy_for_watchduty_tiles(
                 .insert_header(("Content-Type", "application/x-protobuf"))
                 .insert_header(("Cache-Control", "no-cache"))
                 .body(bytes)
-        }
-        Err(err) => {
-            eprintln!("{:#?}", err);
-            HttpResponse::InternalServerError()
-                .insert_header(("Content-Type", "text/plain"))
-                .body("Could not fetch data")
-        }
-    }
-}
-
-#[actix_web::get("/maptiler_terrain_tiles_proxy/{z}/{x}/{y}.webp")]
-pub async fn proxy_for_maptiler_terrain_tiles(
-    path: web::Path<(u8, u32, u32)>,
-    req: HttpRequest,
-) -> impl Responder {
-    let (z, x, y) = path.into_inner();
-
-    let client = reqwest::Client::builder().build().unwrap();
-
-    let api_keys = ["JfNvPTYZZ91w2EXyyJiq", "B265xPhJaYe2kWHOLHTG"];
-
-    let mut rng = rand::thread_rng();
-    let pick_random_key = api_keys[rng.gen_range(0..api_keys.len())];
-
-    let url = format!(
-        "https://api.maptiler.com/tiles/terrain-rgb-v2/{z}/{x}/{y}.webp?key={pick_random_key}&mtsid=23671537-53fa-48f2-9ba1-647a217cbdb1"
-    );
-
-    let request = client
-        .request(reqwest::Method::GET, url)
-        .header("Origin", "https://maps.catenarymaps.org")
-        .header("Referer", "https://maps.catenarymaps.org");
-
-    let response = request.send().await;
-
-    match response {
-        Ok(response) => {
-            let status = response.status();
-
-            //get header content type
-
-            let content_type = match (&response).headers().get("content-type") {
-                Some(content_type) => content_type.to_str().unwrap_or_default(),
-                None => "application/octet-stream",
-            }
-            .to_owned();
-
-            let bytes = response.bytes().await.unwrap();
-
-            match status.is_success() {
-                true => HttpResponse::Ok()
-                    .insert_header(("Content-Type", content_type))
-                    .insert_header(("Cache-Control", "public, max-age=9999999999"))
-                    .insert_header(("Access-Control-Allow-Origin", "*"))
-                    .body(bytes),
-                false => HttpResponse::NotFound()
-                    .insert_header(("Content-Type", content_type))
-                    .body(bytes),
-            }
-        }
-        Err(err) => {
-            eprintln!("{:#?}", err);
-            HttpResponse::InternalServerError()
-                .insert_header(("Content-Type", "text/plain"))
-                .body("Could not fetch data")
-        }
-    }
-}
-
-#[actix_web::get("/terrain_tiles_proxy_aws/{z}/{x}/{y}")]
-pub async fn proxy_for_aws_terrain_tiles(
-    path: web::Path<(u8, u32, u32)>,
-    req: HttpRequest,
-) -> impl Responder {
-    let (z, x, y) = path.into_inner();
-
-    let client = reqwest::Client::builder().build().unwrap();
-
-    let url = format!("https://elevation-tiles-prod.s3.amazonaws.com/v2/terrarium/{z}/{x}/{y}.png");
-
-    let request = client
-        .request(reqwest::Method::GET, url)
-        .header("Origin", "https://maps.catenarymaps.org")
-        .header("Referer", "https://maps.catenarymaps.org");
-
-    let response = request.send().await;
-
-    match response {
-        Ok(response) => {
-            let status = response.status();
-
-            //get header content type
-
-            let content_type = match (&response).headers().get("content-type") {
-                Some(content_type) => content_type.to_str().unwrap_or_default(),
-                None => "application/octet-stream",
-            }
-            .to_owned();
-
-            let bytes = response.bytes().await.unwrap();
-
-            match status.is_success() {
-                true => HttpResponse::Ok()
-                    .insert_header(("Content-Type", content_type))
-                    .insert_header(("Cache-Control", "public, max-age=6048000"))
-                    .insert_header(("Access-Control-Allow-Origin", "*"))
-                    .body(bytes),
-                false => HttpResponse::NotFound()
-                    .insert_header(("Content-Type", content_type))
-                    .body(bytes),
-            }
         }
         Err(err) => {
             eprintln!("{:#?}", err);
---
+++ src/birch/terrain_tiles_proxy.rs
@@ -0,0 +1,62 @@
+use actix_web::Responder;
+use actix_web::{web, HttpRequest, HttpResponse};
+use rand::Rng;
+
+#[actix_web::get("/maptiler_terrain_tiles_proxy/{z}/{x}/{y}.webp")]
+pub async fn proxy_for_maptiler_terrain_tiles(
+    path: web::Path<(u8, u32, u32)>,
+    req: HttpRequest,
+) -> impl Responder {
+    let (z, x, y) = path.into_inner();
+
+    let client = reqwest::Client::builder().build().unwrap();
+
+    let api_keys = ["JfNvPTYZZ91w2EXyyJiq", "B265xPhJaYe2kWHOLHTG"];
+
+    let mut rng = rand::rng();
+    let pick_random_key = api_keys[rng.random_range(0..api_keys.len())];
+
+    let url = format!(
+        "https://api.maptiler.com/tiles/terrain-rgb-v2/{z}/{x}/{y}.webp?key={pick_random_key}&mtsid=23671537-53fa-48f2-9ba1-647a217cbdb1"
+    );
+
+    let request = client
+        .request(reqwest::Method::GET, url)
+        .header("Origin", "https://maps.catenarymaps.org")
+        .header("Referer", "https://maps.catenarymaps.org");
+
+    let response = request.send().await;
+
+    match response {
+        Ok(response) => {
+            let status = response.status();
+
+            //get header content type
+
+            let content_type = match (&response).headers().get("content-type") {
+                Some(content_type) => content_type.to_str().unwrap_or_default(),
+                None => "application/octet-stream",
+            }
+            .to_owned();
+
+            let bytes = response.bytes().await.unwrap();
+
+            match status.is_success() {
+                true => HttpResponse::Ok()
+                    .insert_header(("Content-Type", content_type))
+                    .insert_header(("Cache-Control", "public, max-age=9999999999"))
+                    .insert_header(("Access-Control-Allow-Origin", "*"))
+                    .body(bytes),
+                false => HttpResponse::NotFound()
+                    .insert_header(("Content-Type", content_type))
+                    .body(bytes),
+            }
+        }
+        Err(err) => {
+            eprintln!("{:#?}", err);
+            HttpResponse::InternalServerError()
+                .insert_header(("Content-Type", "text/plain"))
+                .body("Could not fetch data")
+        }
+    }
+}
