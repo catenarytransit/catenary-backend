5e9ee7ce2e377ddad5801889a7c048f2be3c6680
Kyler Chin <kyler@catenarymaps.org>
2025-03-17 21:09:11 -0700

    block neb eisenbahn

--- src/maple/gtfs_handlers/gtfs_de_cleanup.rs
+++ src/maple/gtfs_handlers/gtfs_de_cleanup.rs
@@ -13,6 +13,7 @@ pub fn gtfs_de_cleanup(gtfs: Gtfs) -> Gtfs {
         "Verkehrsverbund Rhein-Sieg",
         "Flixtrain",
         "Verkehrsverbund Rhein-Neckar",
+        "NEB Niederbarnimer Eisenbahn"
     ];
 
     let agency_ids_to_remove = to_delete_agencies
