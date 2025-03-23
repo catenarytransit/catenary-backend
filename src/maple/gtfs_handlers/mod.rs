5c053ce67c202a9b56a0f1594ccd70c99fc44fed
Kyler Chin <kyler@catenarymaps.org>
2025-03-23 16:56:47 -0700

    remove more bodensee

--- src/maple/gtfs_handlers/mod.rs
+++ src/maple/gtfs_handlers/mod.rs
@@ -7,5 +7,6 @@ pub mod hull_from_gtfs;
 pub mod rename_route_labels;
 pub mod shape_colour_calculator;
 pub mod stops_associated_items;
+pub mod remove_agencies;
 
 pub const MAPLE_INGESTION_VERSION: i32 = 13;
