9105537e62ee8ddaa2c2356d3dc743ba94c0e53e
Kyler Chin <kyler@catenarymaps.org>
2025-03-23 16:24:37 -0700

    remove more agencies

--- src/maple/gtfs_handlers/gtfs_de_cleanup.rs
+++ src/maple/gtfs_handlers/gtfs_de_cleanup.rs
@@ -19,7 +19,8 @@ pub fn gtfs_de_cleanup(gtfs: Gtfs) -> Gtfs {
         "VGM/VRL",
         "metronom",
         "DB ZugBus Regionalverkehr Alb-Bodensee",
-        "Südwestdeutsche Verkehrs-AG"
+        "Südwestdeutsche Verkehrs-AG",
+        "Regionalverkehr Alb-Bodensee"
     ];
 
     let agency_ids_to_remove = to_delete_agencies
