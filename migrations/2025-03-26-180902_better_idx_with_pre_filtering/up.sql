3d476f7118ae76980c436bada7349f2b5c54c4bb
Kyler Chin <kyler@catenarymaps.org>
2025-03-26 11:50:21 -0700

    update idx

--- migrations/2025-03-26-180902_better_idx_with_pre_filtering/up.sql
+++ migrations/2025-03-26-180902_better_idx_with_pre_filtering/up.sql
@@ -3,7 +3,7 @@ DROP INDEX  IF EXISTS shapes_linestring_index_intercity_rail ;
 DROP INDEX  IF EXISTS shapes_linestring_index_local_rail ;
 DROP INDEX IF EXISTS shapes_linestring_index_ferry;
 DROP INDEX IF EXISTS  shapes_linestring_not_bus ;
-DROP INDEX   IF EXISTS shapes_linestring_bus;
+DROP INDEX  IF EXISTS shapes_linestring_bus;
 CREATE INDEX shapes_linestring_index_intercity_rail ON gtfs.shapes USING GIST (linestring) WHERE route_type = 2 and allowed_spatial_query = true;
 CREATE INDEX shapes_linestring_index_local_rail ON gtfs.shapes USING GIST (linestring) WHERE route_type IN (0,1,5,7,11,12) and allowed_spatial_query = true;
 CREATE INDEX shapes_linestring_index_ferry ON gtfs.shapes USING GIST (linestring) WHERE route_type = 4 AND allowed_spatial_query = true;
