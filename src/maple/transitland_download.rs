5384de05b79db997228e984824651bca4a0b8d8d
Kyler Chin <kyler@catenarymaps.org>
2025-04-03 14:28:46 -0700

    timeout after 30 min

--- src/maple/transitland_download.rs
+++ src/maple/transitland_download.rs
@@ -27,8 +27,8 @@ fn make_reqwest_client() -> reqwest::Client {
     reqwest::ClientBuilder::new()
         .use_rustls_tls()
         .user_agent("Catenary Maple")
-        //timeout queries after 4 minutes
-        .timeout(Duration::from_secs(60 * 8))
+        //timeout queries after 30 minutes
+        .timeout(Duration::from_secs(60 * 30))
         .connect_timeout(Duration::from_secs(20))
         .danger_accept_invalid_certs(true)
         .deflate(true)
