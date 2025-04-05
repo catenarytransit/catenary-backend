6d61256b81b0bfbd3a1f3246f45e560d06aaade0
Kyler Chin <kyler@catenarymaps.org>
2025-04-05 13:51:33 -0700

    login for austria

--- src/aspen/main.rs
+++ src/aspen/main.rs
@@ -339,8 +339,6 @@ impl AspenRpc for AspenServer {
                                 }
                             }
                         }
-
-                       
                     }
                 }
             }
