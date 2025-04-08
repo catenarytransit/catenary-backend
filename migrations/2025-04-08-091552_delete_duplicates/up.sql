62e4509b1b3c4065ecbe47a3962bf13823af05d7
Kyler Chin <kyler@catenarymaps.org>
2025-04-08 02:36:29 -0700

    duplicate idx deleted

---
+++ migrations/2025-04-08-091552_delete_duplicates/up.sql
@@ -0,0 +1,7 @@
+-- Your SQL goes here
+DROP INDEX IF EXISTS gtfs.chateau_itin_pattern_stop_id_idx;
+DROP INDEX IF EXISTS gtfs.chateau_itin_pattern_idx;
+DROP INDEX IF EXISTS gtfs.chateau_itin_pattern_idx;
+DROP INDEX IF EXISTS gtfs.calendar_chateau;
+DROP INDEX IF EXISTS gtfs.calendar_dates_chateau;
+DROP INDEX IF EXISTS gtfs.direction_pattern_chateau_idx;
