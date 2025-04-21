8f0f8c8c269af4f0031fcfbfcde81ec8d4cf1539
Kyler Chin <kyler@catenarymaps.org>
2025-04-21 16:10:55 -0700

    mvv ruftaxi

--- src/maple/gtfs_handlers/gtfs_de_cleanup.rs
+++ src/maple/gtfs_handlers/gtfs_de_cleanup.rs
@@ -35,6 +35,7 @@ pub fn gtfs_de_cleanup(gtfs: Gtfs) -> Gtfs {
         "Bus München",
         "NachtTram München",
         "NachtBus München",
+        "MVV-Ruftaxi"
     ];
 
     let to_delete_agencies = Vec::from(to_delete_agencies)
