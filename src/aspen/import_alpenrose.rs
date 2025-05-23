7ae60049c5bd9638ed65081d0d3e9589300a9ece
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2025-05-23 10:20:21 -0700

    sort on each item correctly

--- src/aspen/import_alpenrose.rs
+++ src/aspen/import_alpenrose.rs
@@ -1624,8 +1624,14 @@ pub async fn new_rt_data(
     vehicle_routes_cache.shrink_to_fit();
     trip_updates.shrink_to_fit();
 
-    trip_updates_lookup_by_trip_id_to_trip_update_ids.sort();
-    trip_updates_lookup_by_trip_id_to_trip_update_ids.dedup();
+    trip_updates_lookup_by_trip_id_to_trip_update_ids.iter_mut(|(_, x)|
+        {
+            if x.len() > 1 {
+                x.sort();
+                x.dedup();
+            }
+        }
+    );
 
     trip_updates_lookup_by_trip_id_to_trip_update_ids.shrink_to_fit();
     alerts.shrink_to_fit();
