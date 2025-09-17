8e6d1103c1e043bb0852b19917eaf0765e3ac86d
Kyler Chin <kyler@catenarymaps.org>
2025-09-16 17:11:19 -0700

    route type fixer for la metro

--- src/aspen/import_alpenrose.rs
+++ src/aspen/import_alpenrose.rs
@@ -995,7 +995,7 @@ pub async fn new_rt_data(
                             })
                             .flatten();
 
-                        let pos_aspenised = AspenisedVehiclePosition {
+                        let mut pos_aspenised = AspenisedVehiclePosition {
                             trip: vehicle_pos
                                 .trip
                                 .as_ref()
@@ -1102,6 +1102,61 @@ pub async fn new_rt_data(
                             occupancy_percentage: vehicle_pos.occupancy_percentage,
                             congestion_level: vehicle_pos.congestion_level,
                         };
+
+                        if realtime_feed_id.as_str() == "f-metro~losangeles~rail~rt" {
+                            if vehicle_pos.trip.is_none() {
+                                if let Some(vehicle_info) = vehicle_pos.vehicle.as_ref() {
+                                    if let Some(vehicle_id) = &vehicle_info.id {
+                                        match vehicle_id.contains("-") {
+                                            true => {
+                                                let split = vehicle_id.split('-').collect::<Vec<&str>>();
+
+                                                let attempted_number = vehicle_id.parse::<u32>();
+
+                                                if let Ok(attempted_number) = attempted_number {
+                                                    let new_route_type = match attempted_number {
+                                                        //Siemens P2000
+                                                        201..=250 => 0,
+                                                        301..=302 => 0,
+                                                        //AnsaldoBreda P2550
+                                                        701..=750 => 0,
+                                                        1001..=1235 => 0,
+                                                        //Breda A650
+                                                        500..=699 => 1,
+                                                        4000..=4064 => 1,
+                                                        _ => 0
+                                                    };
+
+                                                    pos_aspenised.route_type = new_route_type;
+                                                }
+                                            },
+                                            false => {
+                                                let attempted_number = vehicle_id.parse::<u32>();
+
+                                                if let Ok(attempted_number) = attempted_number {
+                                                    let new_route_type = match attempted_number {
+                                                        //A Line
+                                                        100..=199 => 0,
+                                                        //B & D Lines
+                                                        200..=299 => 1,
+                                                        //E Line
+                                                        400..=499 => 0,
+                                                        //K Line
+                                                        700..=799 => 0,
+                                                        //C Line
+                                                        300..=399 => 0,
+                                                        //700
+                                                        _ => 0
+                                                    };
+
+                                                    pos_aspenised.route_type = new_route_type;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
 
                         let pos_aspenised = vehicle_pos_supplement(
                             pos_aspenised,
