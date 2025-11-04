7c05495c4cc7d8cb322381281164ff75f0af6d93
Kyler Chin <kyler@catenarymaps.org>
2025-11-04 08:38:22 -0800

    Update main.rs

--- src/maple/girolle/main.rs
+++ src/maple/girolle/main.rs
@@ -103,7 +103,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Sync + Send>> {
                         data_about_feed.time_to_download_ms = Some(duration.as_millis() as u64);
 
                         if let Ok(bytes) = bytes {
-                            let data = bytes_result.as_ref();
+                            let data = bytes.as_ref();
                             let hash = seahash::hash(data);
 
                             data_about_feed.seahash = Some(hash);
