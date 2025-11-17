2b4f1938b2522323c7ec278955778a0185d9935c
Kyler Chin <kyler@catenarymaps.org>
2025-11-16 21:11:02 -0800

    use another api key found on github

--- src/alpenrose/custom_rt_feeds/chicagotransit.rs
+++ src/alpenrose/custom_rt_feeds/chicagotransit.rs
@@ -15,6 +15,7 @@ pub async fn fetch_chicago_data(
         "13f685e4b9054545b19470556103ec73",
         "c08cdb1cc63a40f186bc9f960deeb5fd",
         "33305d8dcece4aa58c651c740f88d1e2",
+        "3106a8d27f8b4f8fb99b7c8d895163dd",
     ];
 
     let randomly_picked_api_key = rand::random_range(0..api_keys.len());
