d673b51052763f0a11459107b0c8e07dfec4ad30
Kyler Chin <kyler@catenarymaps.org>
2026-01-09 23:34:41 -0800

    add another feed to pfaedle

--- src/maple/gtfs_process.rs
+++ src/maple/gtfs_process.rs
@@ -182,6 +182,7 @@ pub async fn gtfs_process_feed(
         | "f-rhdf62com~seau~interurbain~fr"
         | "f-rhdf80com~seau~interurbain~fr"
         | "f-rhdf80sco~seau~scolaire~fr"
+        | "f-auvergne~rhône~alpes~oùra"
         | "f-hauts~de~france~loise~1" => {
             let _ = execute_pfaedle_rs(
                 path.as_str(),
