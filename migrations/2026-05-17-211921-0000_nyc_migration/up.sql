4eb7beef60438d84cf93ec3fff20ef69bb2f46c5
Kyler Chin <kyler@catenarymaps.org>
2026-05-17 14:30:49 -0700

    remove concurrently from migration

--- migrations/2026-05-17-211921-0000_nyc_migration/up.sql
+++ migrations/2026-05-17-211921-0000_nyc_migration/up.sql
@@ -1,4 +1,4 @@
-CREATE INDEX CONCURRENTLY IF NOT EXISTS trips_compressed_nyct_rt_suffix_idx
+CREATE INDEX IF NOT EXISTS trips_compressed_nyct_rt_suffix_idx
 ON gtfs.trips_compressed (
   route_id,
   (substr(trip_id, strpos(trip_id, '_') + 1)),
