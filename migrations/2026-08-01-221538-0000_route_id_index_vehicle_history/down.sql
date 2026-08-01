1f99f0b5b18b05ccfc7bd349c6efd1e38cd6f420
Kyler Chin <kyler@catenarymaps.org>
2026-08-01 15:17:08 -0700

    vehicle history route index

---
+++ migrations/2026-08-01-221538-0000_route_id_index_vehicle_history/down.sql
@@ -0,0 +1,2 @@
+-- This file should undo anything in `up.sql`
+DROP INDEX IF EXISTS route_id_vehicle_history_index;
---
+++ migrations/2026-08-01-221538-0000_route_id_index_vehicle_history/up.sql
@@ -0,0 +1,2 @@
+-- Your SQL goes here
+CREATE INDEX route_id_vehicle_history_index ON gtfs.basic_vehicle_history (chateau, route_id);
