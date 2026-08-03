3ec4e381a8446145dee5f3070a5031fe4f2a8a13
Kyler Chin <kyler@catenarymaps.org>
2026-08-02 23:02:06 -0700

    Bratislava pfaedle

--- src/maple/gtfs_process.rs
+++ src/maple/gtfs_process.rs
@@ -811,6 +811,16 @@ pub async fn gtfs_process_feed(
             )
             .await?;
         }
+        "f-u2s1vm0ergtc~bratislava" | "f-bratislave~regionálnej" => {
+            let _ = execute_pfaedle_rs(
+                path.as_str(),
+                "./bratislava.osm.pbf",
+                None,
+                true,
+                true,
+            )
+            .await?;
+        }
         /*"f-u0-switzerland" => {
             // 2 passes requried
             let _ = execute_pfaedle_rs(
